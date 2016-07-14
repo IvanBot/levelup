42d5cc500664a3a259162af14cbeee183148da4c
Алёна Булатова <alien89yar@gmail.com>
2016-07-14 15:52:22 +0300

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,47 +1,14 @@
 create table levelup76;
 
 use levelup76;
-
-create table users (
-id int unsigned AUTO_INCREMENT PRIMARY KEY,
-phone bigint unsigned NOT NULL,
-username TINYTEXT,
-surname TINYTEXT,
-ip TINYTEXT,
-email TINYTEXT,
-userpassword TINYTEXT,
-permission TINYINT,
-usercomment TEXT);
-
-create table 
-activity (
-id int unsigned AUTO_INCREMENT PRIMARY KEY,
-trainer_id int unsigned,
-activityname TEXT,
-activitycomment TEXT,
-activityduration int,
-maxcount int,
-mincount int);
-
-create table 
-trainers (
-id int unsigned AUTO_INCREMENT PRIMARY KEY,
-phone bigint unsigned,
-trainername TINYTEXT,
-trainersurname TINYTEXT,
-experience DATE,
-email TINYTEXT,
-photo TEXT,
-userpassword TINYTEXT,
-permission TINYINT,
-active TINYINT,
-trainercomment TEXT);
-
-create table 
+create table
 record_activity (
 id int unsigned AUTO_INCREMENT PRIMARY KEY,
 user_id int unsigned,
 schedule_id int unsigned,
+activity_id int unsigned,
+activitydate DATE,
+starttime TIME,
 recordcomment TEXT);
 
 create table 
@@ -49,8 +16,13 @@ schedule_activity (
 id int unsigned AUTO_INCREMENT PRIMARY KEY,
 activity_id int unsigned,
 trainer_id int unsigned,
-activitytime TINYINT,
+starttime TIME,
+endtime TIME,
 activityduration TINYINT,
 activitydate DATE,
+startdate DATE,
+mincount int,
 maxcount int,
-mincount int);
+cycleday TINYINT,
+deleted TINYINT
+);
