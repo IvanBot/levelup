245523e504beb9e1357b0aa8c89f7e9d2d8cb7bf
Алёна Булатова <alien89yar@gmail.com>
2016-07-14 15:53:41 +0300

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,5 +1,40 @@
 create table levelup76;
+create table users (
+ id int unsigned AUTO_INCREMENT PRIMARY KEY,
+ phone bigint unsigned NOT NULL,
+ username TINYTEXT,
+ surname TINYTEXT,
+ ip TINYTEXT,
+ email TINYTEXT,
+ userpassword TINYTEXT,
+ permission TINYINT,
+usercomment TEXT);
 
+create table 
+activity (
+id int unsigned AUTO_INCREMENT PRIMARY KEY,
+trainer_id int unsigned,
+activityname TEXT,
+activitycomment TEXT,
+activityduration int,
+maxcount int,
+mincount int);
+ 
+create table 
+trainers (
+id int unsigned AUTO_INCREMENT PRIMARY KEY,
+phone bigint unsigned,
+trainername TINYTEXT,
+trainersurname TINYTEXT,
+experience DATE,
+email TINYTEXT,
+photo TEXT,
+userpassword TINYTEXT,
+permission TINYINT,
+active TINYINT,
+trainercomment TEXT);
+ 
+ 
 use levelup76;
 create table
 record_activity (
