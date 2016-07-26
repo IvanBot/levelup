c7c842e37ed2603e72efbb03cc0f0332a44c73c1
Алёна Булатова <alien89yar@gmail.com>
2016-07-26 14:48:38 +0300

    Upload new file

---
+++ coaches/index.php
@@ -0,0 +1,198 @@
+<?require_once($_SERVER['DOCUMENT_ROOT'] .'/header.php') ?>
+    <br>
+    <!-- Тренеры -->
+    <div class="img_position">
+        <div class="container">
+            <div class="row masonry" data-columns>
+
+
+                <div class="item">
+                    <div class="thumbnail">
+                        <img src="/img/moskalev.png" class="img-responsive" alt="Responsive image" id="img_coach">
+                        <h4 class="coach_name">Андрей Москалёв</h4>
+                        <p class="coach_prof">"Фриран"</p>
+                        <p class="coach_prof">Взрослые группы (от 14 лет)</p>
+                        <p class="coach_prof">Индивидуальные тренировки</p>
+                        <p class="coach_work">стаж 3 года<br><br><br></p>
+                    </div>
+                </div>
+
+                <div class="item">
+                    <div class="thumbnail">
+                        <img src="/img/astahova.png" class="img-responsive" alt="Responsive image" id="img_coach">
+                        <h4 class="coach_name">Мария Астахова</h4>
+                        <p class="coach_prof"> "Прыжки на батуте" (trampolining)</p>
+                        <p class="coach_prof">Детские группы</p>
+                        <p class="coach_prof">Индивидуальные тренировки</p>
+                        <p class="coach_work">Мастер спорта<br> по прыжкам на батуте<br><br></p>
+                    </div>
+                </div>
+
+                <div class="item">
+                    <div class="thumbnail">
+                        <img src="/img/romanov.png" class="img-responsive" alt="Responsive image" id="img_coach">
+                        <h4 class="coach_name">Иван Романов</h4>
+                        <p class="coach_prof">"Спортивная акробатика"</p>
+                        <p class="coach_prof">"Прыжки на батуте" (trampolining) </p>
+                        <p class="coach_prof">Индивидуальные тренировки</p>
+                        <p class="coach_work">КМС по спортивной акробатике<br>
+                            С 2004 г. по н.вр. профессиональный артист цирка</p>
+                    </div>
+                </div>
+
+                <div class="item">
+                    <div class="thumbnail">
+                        <img src="/img/vlasenko.png" class="img-responsive" alt="Responsive image" id="img_coach">
+                        <h4 class="coach_name">Ольга Власенко</h4>
+                        <p class="coach_prof">Детские группы с 5 лет</p>
+                        <p class="coach_work">стаж 4 года</p>
+                    </div>
+                </div>
+
+                <div class="item">
+                    <div class="thumbnail">
+                        <img src="/img/rodina.png" class="img-responsive" alt="Responsive image" id="img_coach">
+                        <h4 class="coach_name">Анастасия Родина</h4>
+                        <p class="coach_prof">Батутный фристайл</p>
+                        <p class="coach_work">стаж 1 год</p>
+                    </div>
+                </div>
+
+                <div class="item">
+                    <div class="thumbnail">
+                        <img src="/img/sogomonov.png" class="img-responsive" alt="Responsive image" id="img_coach">
+                        <h4 class="coach_name">Дмитрий Согомонов</h4>
+                        <p class="coach_prof">Батутный фристайл</p>
+                        <p class="coach_work">стаж 6 лет</p>
+                    </div>
+                </div>
+
+                <div class="item">
+                    <div class="thumbnail">
+                        <img src="/img/gribkov.png" class="img-responsive" alt="Responsive image" id="img_coach">
+                        <h4 class="coach_name">Александр Грибков</h4>
+                        <p class="coach_prof">"Фриран"</p>
+                        <p class="coach_prof">Взрослые группы (от 14 лет)</p>
+                        <p class="coach_prof">Индивидуальные тренировки</p>
+                        <p class="coach_work">стаж 3 года</p>
+                    </div>
+                </div>
+
+
+
+                <div class="item">
+                    <div class="thumbnail">
+                        <img src="/img/gasymov.png" class="img-responsive" alt="Responsive image" id="img_coach">
+                        <h4 class="coach_name">Назир Гасымов</h4>
+                        <p class="coach_prof">"Фриран"</p>
+                        <p class="coach_prof">Взрослые группы (от 14 лет)</p>
+                        <p class="coach_prof">Индивидуальные тренировки</p>
+                        <p class="coach_work">стаж 2 года</p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+    <!-- Конец блока тренеров -->
+
+
+
+
+    <br><br>
+
+    <div class="container">
+        <div class="row name_price">
+            <div class="name_price"><a name="parthers"></a>Спортивная школа</div>
+            <span class="cherta_ind hidden-xs"></span>
+        </div>
+    </div>
+
+
+    <!-- Таблица -->
+    <div class="container">
+        <div class="row schedule">
+            <h>Расписание занятий</h><br>
+        </div>
+    </div>
+    <br>
+
+    <div class="container">
+        <div class="row masonry" data-columns>
+
+
+            <div class="item">
+                <div class="schedule_th">
+                    <h4 id="sche_item_b">Понедельник</h4>
+                    <p id="sche_item">9:45-11:15<br>Фриран 1-я гр. Москалев А.</p>
+                    <p id="sche_item">11:30-13:00<br>Фриран 2-я гр. Москалев А.</p>
+                    <p id="sche_item">17:00-18:30<br>Батутный фристайл Согомонов Д.</p>
+                </div>
+            </div>
+
+            <div class="item">
+                <div class="schedule_th">
+                    <h4 id="sche_item_b">Вторник</h4>
+                    <p id="sche_item">11:30-13:00<br>Детская 1-я гр. Астахова М.</p>
+                    <p id="sche_item">13:30-15:00<br>Детская 2-я гр. Власенко О.</p>
+                    <p id="sche_item">15:15-16:45<br>Батутный фристайл Согомонов Д.</p>
+                </div>
+            </div>
+
+            <div class="item">
+                <div class="schedule_th">
+                    <h4 id="sche_item_b">Среда</h4>
+                    <p id="sche_item">8:00-9:30<br>Батутный фристайл Родина А.</p>
+                    <p id="sche_item">9:45-11:15<br>Взрослая 1-я гр. Власов М.</p>
+                    <p id="sche_item">15:15-16:45<br>Батутный фристайл Согомонов Д.</p>
+                </div>
+            </div>
+
+            <div class="item">
+                <div class="schedule_th">
+                    <h4 id="sche_item_b">Четверг</h4>
+                    <p id="sche_item">13:30-15:00<br>Фриран 1-я гр. Москалев А.</p>
+                    <p id="sche_item">15:15-16:45<br>Фриран 2-я гр. Москалев А.</p>
+                    <p id="sche_item">17:00-18:30<br>Взрослая 2-я гр. Власов М.</p>
+                </div>
+            </div>
+
+            <div class="item">
+                <div class="schedule_th">
+                    <h4 id="sche_item_b">Пятница</h4>
+                    <p id="sche_item">8:00-9:30<br>Батутный фристайл Родина А.</p>
+                    <p id="sche_item">11:30-13:00<br>Детская 1-я гр. Астахова М.</p>
+                    <p id="sche_item">13:30-15:00<br>Детская 2-я гр. Власенко О.</p>
+                </div>
+            </div>
+
+            <div class="item">
+                <div class="schedule_th">
+                    <h4 id="sche_item_b">Суббота</h4>
+                    <p id="sche_item">9:45-11:15<br>Взрослая 1-я гр. Власов М.</p>
+                    <p id="sche_item">11:30-13:00<br>Взрослая 2-я гр. Власов М.</p>
+                    <p id="sche_item">15:15-16:45<br>Батутный фристайл Согомонов Д.</p>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <br>
+    <br>
+    <div class="container">
+        <div class="row" id="tab_text_down">
+                <span>ВНИМАНИЕ: Занятия проводятся в спортивной или удобной форме одежды, без ремней, пряжек. Идеально подходят длинные, закрывающие колени шорты и футболка. На ногах нескользящие носки или прыжковые с силиконовой подошвой.
+                <br>
+                <br>
+                Мы допускаем к занятиям детей согласно возрастов:<br>
+                - индивидуальная тренировка – от 5 лет;<br>
+                - групповая тренировка – от 6 лет;<br>
+                - детские праздники – от 7 лет.
+                </span>
+        </div>
+    </div>
+    <br>
+    <!-- Конец таблицы -->
+<?require_once($_SERVER['DOCUMENT_ROOT'] .'/footer.php') ?>
