72ba1b37a0ca8d3257c27cc77058faa7d9453059
David Mackmudov <dev4@erpico.ru>
2016-08-08 12:05:43 +0300

    Админка db

--- src/db.php
+++ src/db.php
@@ -1,21 +1,21 @@
 <?
 
-$fcfg = fopen("/etc/erpico.conf", "r");
+/*$fcfg = fopen("/etc/erpico.conf", "r");
 while ($s = fgets($fcfg)) {
  list($key,$value) = explode("=", $s, 2);
  $key = trim($key);
  $value = trim($value, " \"\t\n\r\0\x0B");
  $config[$key] = $value;
 };
-fclose($fcfg);
+fclose($fcfg);*/
 
 /**/
-/*
-$config['db_host'] = "192.168.137.10:3306";
+
+$config['db_host'] = "127.0.0.1";
 $config['db_user'] = "root";
-$config['db_password'] = "dtpltghjqltv";
+$config['db_password'] = "";
 $config['db_schema'] = "levelup76";
-*/
+
 
 $db = mysql_connect($config['db_host'],$config['db_user'],$config['db_password']);
 if (!$db) die(mysql_error());
